90895b60727b6d083f850103f20ef0b15e65dded
Nguyễn Thành Đạo <110709579+thanhdaonguyen@users.noreply.github.com>
2024-10-28 21:29:57 +0700

    add cors

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.main import router
 import uvicorn
 
 app = FastAPI(
     title="Uet CryptoTool",
     summary="CryptoTool",
+)
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # List of allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
 )
 app.include_router(router)
 
